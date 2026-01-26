4859a51ea03fdec8e5e716258718eb62edb5f6c7
SiulDesing <siuldesing@gmail.com>
2026-01-25 20:34:37 -0400

    Cambios en la UI y UX

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primario: "var(--color-primario)",
+        secundario: "var(--color-secundario)",
+        peligro: "var(--color-peligro)",
+        fondo: "var(--color-fondo)",
+        "texto-oscuro": "var(--texto-oscuro)",
+        translucido: "var(--color-fondo-translucido)"
+      },
+      borderRadius: {
+        estandar: "var(--borde-radio)"
+      }
+    }
   },
   plugins: []
 };
