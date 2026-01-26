15ce5ce83597e405efd142425786d85b2d5878b4
SiulDesing <siuldesing@gmail.com>
2026-01-25 20:38:20 -0400

    Actualiza estilos de lista de tareas para mejorar la apariencia y la consistencia visual.

--- src/scripts/todoLogic.js
+++ src/scripts/todoLogic.js
@@ -33,7 +33,7 @@ function renderizarLista() {
 
   for (let i = 0; i < tareas.length; i++) {
     listaUI.innerHTML += `
-    <li class="bg-white border-b-2 border-gray-200 p-3 flex justify-between items-center" style="animation: aparecer 0.4s linear forwards;">
+    <li class="text-secundario bg-translucido border-b-2 border-translucido p-3 mb-2 flex justify-between items-center" style="animation: aparecer 0.4s linear forwards;">
         <span>${tareas[i].texto}</span>
         <button class="bg-none border-none cursor-pointer p-1.25 transition-transform duration-200 hover:scale-110" onclick="borrarTarea(${tareas[i].id})"><i class='bx bx-trash text-xl text-red-600'></i></button>
     </li>
